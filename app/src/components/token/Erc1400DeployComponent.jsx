aaf1971fbdaf80549310861a2f70ae550f609b5f
baiyuqi <1353813001@qq.com>
2019-11-08 15:48:04 +0800

    lll

--- app/src/components/token/Erc1400DeployComponent.jsx
+++ app/src/components/token/Erc1400DeployComponent.jsx
@@ -27,11 +27,11 @@ export default class Erc1400Component extends Component {
     event.preventDefault();
     let web3 = this.props.drizzle.web3;
     this.MyContract.setProvider(web3.currentProvider);
-    if(this.formData.name == undefined ||this.formData.symbol == undefined ||this.formData.decimals == undefined){
+    if (this.formData.name == undefined || this.formData.symbol == undefined || this.formData.decimals == undefined) {
       alert("select token please!")
       return
     }
-    if(this.formData.name.length == 0 ||this.formData.symbol.length == 0 ||this.formData.decimals.length == 0){
+    if (this.formData.name.length == 0 || this.formData.symbol.length == 0 || this.formData.decimals.length == 0) {
       alert("select token please!")
       return
     }
@@ -47,19 +47,43 @@ export default class Erc1400Component extends Component {
       this.utils.toBN(this.formData.decimals),
       this.formData.controllers,
       this.props.drizzle.contracts.ComplianceServiceRegistry.address, { from: this.props.drizzleState.accounts[0] }).then(inst => {
-      let payload = {
+        let payload = {
           name: this.formData.name,
           symbol: this.formData.symbol,
           decimals: this.utils.toBN(this.formData.decimals),
           controllers: this.formData.controllers.slice(0),
           registryAddress: this.props.drizzle.contracts.ComplianceServiceRegistry.address,
           contractAddress: inst.address,
-          deployAccount: this.props.drizzleState.accounts[0]
+          deployAccount: this.props.drizzleState.accounts[0],
+          tokenType: "erc1400"
         };
         this.props.drizzle.store.dispatch(deployFinished1400(payload))
+        this.save(payload)
       })
 
     return;
+  }
+ async save(token) {
+    try {
+      let response = await fetch("http://localhost:8080/saveDeployedToken", {
+        body: JSON.stringify(token), // must match 'Content-Type' header
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, same-origin, *omit
+        headers: {
+          'user-agent': 'Mozilla/4.0 MDN Example',
+          'content-type': 'application/json'
+        },
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // *client, no-referrer
+      })
+      let json = response.json() // parses response to JSON
+    } catch (err) {
+      alert(err);
+    } finally {
+
+    }
   }
 
   handleInputChange(event) {
@@ -72,6 +96,10 @@ export default class Erc1400Component extends Component {
 
   }
   handleAddController(event) {
+    if (!this.utils.isAddress(this.formData.controller)) {
+      alert("input address!")
+      return
+    }
     const value = this.formData.controllers.push(this.formData.controller)
   }
   // string memory name,
@@ -83,7 +111,7 @@ export default class Erc1400Component extends Component {
 
     return (
       <div className="mysection">
-       
+
         <div
           onChange={this.handleInputChange}
         >
